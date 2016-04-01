c3a632fedd628f35cf182094c9fc2250492c0d32
Alex Harvey <alex@razorconsulting.com.au>
2016-04-01 14:43:10 +1100

    Add working acceptance tests.

--- spec/acceptance/management_spec.rb
+++ spec/acceptance/management_spec.rb
@@ -1,17 +1,30 @@
 require 'spec_helper_acceptance'
 
-describe 'rancher::management hostclass' do
-  describe 'running puppet code' do
-    it 'should work with no errors' do
+describe 'rancher::management' do
+  context 'puppet apply' do
+    it 'is expected to apply and be idempotent' do
       pp = <<-EOS
-      class { 'rancher::management':
-        rancher_manager_port => "8080",
-      }
-      EOS
+class { 'rancher::management':
+  rancher_manager_port => '8080',
+}
+EOS
 
-    # Run it twice and test for idempotency
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe docker_container('rancher/server') do
+      it { is_expected.to be_running }
+    end
+
+    context 'is expected to be running on the default port' do
+      before :each do
+        sleep 5 # Give it time to start.
+      end
+
+      describe command('curl 0.0.0.0:8080') do
+        its(:stdout) { is_expected.to match /self.*0.0.0.0:8080/ }
+      end
     end
 
   end
