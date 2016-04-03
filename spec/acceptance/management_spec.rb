f2665525f623f057e6f4a30743055698a97fac3f
Alex Harvey <alex@razorconsulting.com.au>
2016-04-03 20:16:18 +1000

    Add working acceptance test for management.

--- spec/acceptance/management_spec.rb
+++ spec/acceptance/management_spec.rb
@@ -12,16 +12,16 @@ EOS
       apply_manifest(pp, :catch_failures => true)
       apply_manifest(pp, :catch_changes => true)
     end
+  end
 
-    context 'is expected to be running on the default port' do
-      before :each do
-        sleep 5 # Give it time to start.
-      end
+  describe docker_image('rancher/server') do
+    it { is_expected.to exist }
+  end
 
-      describe command('curl 0.0.0.0:8080') do
-        its(:stdout) { is_expected.to match /self.*0.0.0.0:8080/ }
-      end
-    end
-
+  describe command('curl 0.0.0.0:8080') do
+    it {
+      sleep 60 # Give Rancher time to start.
+      expect(subject.stdout).to match /self.*0.0.0.0:8080/
+    }
   end
 end
