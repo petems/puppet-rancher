a7fde8ac2802e6966822cffe2c4e67be67fe2d45
Peter Souter <p.morsou@gmail.com>
2016-04-03 14:55:35 +0100

    Fix node spec test

--- spec/acceptance/node_spec.rb
+++ spec/acceptance/node_spec.rb
@@ -5,8 +5,8 @@ describe 'rancher::node' do
     it 'is expected to apply with no errors' do
       pp = <<-EOS
 class { 'rancher::node':
-  'management'         => '10.0.1.1',
-  'registration_token' => 'DB121CFBA836F9493653:1434085200000:2ZOwUMd6fIzz44efikGhBP1veo',
+  management         => '10.0.1.1',
+  registration_token => 'DB121CFBA836F9493653:1434085200000:2ZOwUMd6fIzz44efikGhBP1veo',
 }
 EOS
 
