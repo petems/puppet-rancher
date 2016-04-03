202e050b73c1a958d4582364c24eb49d18c89a41
Peter Souter <p.morsou@gmail.com>
2016-04-03 15:55:33 +0100

    Adding docker class code in Travis check

--- spec/acceptance/management_spec.rb
+++ spec/acceptance/management_spec.rb
@@ -2,30 +2,40 @@ require 'spec_helper_acceptance'
 
 describe 'rancher::management' do
   context 'puppet apply' do
-    it 'is expected to apply and be idempotent' do
-      pp = <<-EOS
-class { 'rancher::management':
-  rancher_manager_port => '8080',
-}
-EOS
 
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes => true)
+    if ENV['HAS_JOSH_K_SEAL_OF_APPROVAL'] do
+      it 'run docker code first for traivs issues' do
+        pp <<-EOS
+        include docker
+        EOS
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes => true)
+      end
+
+      it 'is expected to apply and be idempotent' do
+        pp = <<-EOS
+        class { 'rancher::management':
+          rancher_manager_port => '8080',
+        }
+        EOS
+
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes => true)
+      end
     end
-  end
 
-  describe command('journalctl -u docker.service') do
-    it {
-      expect(subject.stdout).to match /SHOW ME THE LOGS/
-    }
-  end
+    describe command('journalctl -u docker.service') do
+      it {
+        expect(subject.stdout).to match /SHOW ME THE LOGS/
+      }
+    end
 
-  describe docker_image('rancher/server') do
-    it { is_expected.to exist }
-  end
+    describe docker_image('rancher/server') do
+      it { is_expected.to exist }
+    end
 
-  describe command('curl 0.0.0.0:8080') do
-    it {
+    describe command('curl 0.0.0.0:8080') do
+      it {
       sleep 60 # Give Rancher time to start.
       expect(subject.stdout).to match /self.*0.0.0.0:8080/
     }
